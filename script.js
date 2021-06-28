38ec57e715c28ad970d89c5dbceb486d10a8585c
Tejas1021 <72667701+Tejas1021@users.noreply.github.com>
2021-06-28 12:15:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r=document.getElementById("rate").value;
+    n=document.getElementById("years").value;
     
+    var result=((p*n*r)/100);
+    document.getElementById("result").innerHTML=result;
 }
         

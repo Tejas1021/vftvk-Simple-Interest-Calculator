fa4b483b4d85901c21bd012c22ce0af3372a3364
Tejas1021 <72667701+Tejas1021@users.noreply.github.com>
2021-06-29 12:34:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    r=document.getElementById("rate").value;
-    n=document.getElementById("years").value;
-    
-    var result=((p*n*r)/100);
-    document.getElementById("result").innerHTML=result;
-}
+    {
+        var n = document.querySelector("select").value;
+        var p,r,a;
+        p = document.getElementById("principal").value;
+        if(p=0)
+        {
+            alert("Enter A Positive Number");
+            return false;
+        }
+        r = document.getElementById("rate").value;
+        a = (p*n*r)/100;
+        var b = Number(p)+ Number(a);
+        var s = Number(n)+2021;
         
+        document.getElementById("result").innerHTML = a;
+        document.getElementById("amount").innerHTML = b;
+        document.getElementById("pri").innerHTML = p;
+        document.getElementById("time").innerHTML = s;
+        document.getElementById("ra").innerHTML = r;
+    }
